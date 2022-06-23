b91059574877561f495bcc0d4e2fce9363720dd5
Anup <anuppoudel60@gmail.com>
2022-06-23 14:23:19 +1000

    components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [toDo, setToDo] = useState<string>("");
+  const [toDos, setToDos] = useState<Todo[]>([]);
+
+  const handleOnSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (toDo) {
+      setToDos([...toDos, { id: Date.now(), todo: toDo, isDone: false }]);
+      setToDo("");
+    }
+  };
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField toDo={toDo} setToDo={setToDo} />
+      <InputField
+        toDo={toDo}
+        setToDo={setToDo}
+        handleOnSubmit={handleOnSubmit}
+      />
+      {/* <ToDoList/> */}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
 interface Props {
   toDo: string;
   setToDo: React.Dispatch<React.SetStateAction<string>>;
+  handleOnSubmit: (e: React.FormEvent) => void;
 }
 
-const InputField = ({ toDo, setToDo }: Props) => {
+const InputField: React.FC<Props> = ({
+  toDo,
+  setToDo,
+  handleOnSubmit,
+}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input placeholder="Enter a task" className="inputBox" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleOnSubmit(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        placeholder="Enter a task"
+        className="inputBox"
+        value={toDo}
+        name="toDo"
+        onChange={(e) => setToDo(e.target.value)}
+      />
       <button className="inputSubmit" type="submit">
         ADD
       </button>
